e73c3e672868d0a0f9d84aef9ff28749ec615629
ZGorringe <zacgorringe@gmail.com>
2014-09-30 09:29:23 -0600

    added working fib finder and num finder

--- Fibonacci.js
+++ Fibonacci.js
@@ -5,10 +5,29 @@ emptyArr = [0, 1];
 var fib = function(arr) {
 	var fibArr = [];
 	for (var i = 0; i < 99; i++) {
-	    var n = arr[i] + arr[i + 1];
+	    var n = arr[i] + arr[i + 1]; //This is my function.  I was close.
 		fibArr.push(n);
 		i++;
 	}
 	return fibArr;
+}
+
+var i;
+var fib = [];
+fib[0] = 0;
+fib[1] = 1;
+for(var i = 2; i<=100; i++) { //Code that generates the sequence
+	fib[i] = fib[i-2] + fib[i-1];
+}
+
+console.log(fib);
+
+var find = function(arr) {
+	for (var i = 0; i < arr.length; i++) {//Code that checks for a certain number ('n').
+		if (arr[i] === 'n') {
+			return true;
+		}
+	}
+	return false;
 }
 
